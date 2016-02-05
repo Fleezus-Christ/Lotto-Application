1f1f2729f61d0840c12e5583de47ef4902ab709c
Sharif Uddin <sharifuddin6@outlook.com>
2016-02-05 17:28:35 +0000

    Implemented number generator.

---
+++ LottoGenerator/Model/EuromillionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace LottoGenerator.Model
+{
+    public class EuromillionViewModel
+    {
+        public IEnumerable<int> NumberList { get; set; }
+        public IEnumerable<int> NumberLuckyList { get; set; }
+        public int MatchCount { get; set; }
+    }
+}
--- LottoGenerator/View/EuromillionsView.xaml.cs
+++ LottoGenerator/View/EuromillionsView.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using LottoGenerator.Model;
 
 namespace LottoGenerator.View
 {
@@ -8,6 +14,8 @@ namespace LottoGenerator.View
     /// </summary>
     public partial class EuromillionsView : UserControl
     {
+        private List<EuromillionViewModel> _generatedPick;
+
         public EuromillionsView()
         {
             InitializeComponent();
@@ -15,17 +23,61 @@ namespace LottoGenerator.View
 
         private void GenerateButton_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            _generatedPick = GeneratedPick(10);
+            LottoList.ItemsSource = _generatedPick;
+        }
+
+        private static EuromillionViewModel GeneratePick()
+        {
+            var numbers = Enumerable.Range(1, 50).ToList();
+            var luckyNumbers = Enumerable.Range(1, 11).ToList();
+            var random = new Random();
+            var index = -1;
+
+            for (var i = 0; i < 45; i++)
+            {
+                index = random.Next(numbers.Count);
+                numbers.RemoveAt(index);
+            }
+
+            for (var i = 0; i < 9; i++)
+            {
+                index = random.Next(luckyNumbers.Count);
+                luckyNumbers.RemoveAt(index);
+            }
+
+            return new EuromillionViewModel()
+            {
+                NumberList = numbers,
+                NumberLuckyList = luckyNumbers,
+                MatchCount = 0
+            };
+        }
+
+        private static List<EuromillionViewModel> GeneratedPick(int total)
+        {
+            var generatedPick = new List<EuromillionViewModel>();
+            for (var i = 0; i < total; i++)
+            {
+                generatedPick.Add(GeneratePick());
+            }
+            return generatedPick;
         }
 
         private void ProcessButton_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+
         }
 
         private void LottoList_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            
+        }
+
+        private void AcceptedTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
